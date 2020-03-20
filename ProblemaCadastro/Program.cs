ff3a897d1777aca0ba147d95049a514e6c094a4f
Robson Jesus <stayto.rob@hotmail.com>
2020-03-20 09:47:56 -0300

    Versao 1 Problema cadastro

---
+++ ProblemaCadastro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProblemaCadastro {
+    class Program {
+        static void Main(string[] args) {
+
+            
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int Quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, Quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o numero de produtos a ser adicionado no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o numero de produtos a ser removidos no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
